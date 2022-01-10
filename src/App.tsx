aea31ad38cace7e5ba2d824b742c6b5a8eae0625
Gabriel <gbds1703farra@hotmail.com>
2022-01-09 21:56:58 -0300

    Primeiro commit.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainPage from './pages/mainPage';
+import './styles/global.css';
+
+function App() {
+  return (
+    <>
+      <MainPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/pages/mainPage.css';
+
+function MainPage() {
+    return (
+        <div className="mainPage">
+        </div>
+    )
+}
+
+export default MainPage;
