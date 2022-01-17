c5dd67921427949d75309cbb1e8701f1887a06fc
Gabriel <gbds1703farra@hotmail.com>
2022-01-17 13:46:54 -0300

    Removido borda, adicionado links e corrigido tamanho da mainPage.

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -7,9 +7,9 @@ function Contact() {
             <h1>Contato!</h1>
             <h2>Tem uma ideia e gostaria de torná-la realidade?<br />Entre em contato, posso te ajudar.</h2>
             <p>Envie uma mensagem</p>
-            <p>Email: this.dev.gabriel@gmail.com</p>
-            <p>WhatsApp: (11)95629-5523</p>
-            <p>Instagram: @this.dev.gabriel</p>
+            <p>Email: <a href="mailto:this.dev.gabriel@gmail.com">this.dev.gabriel@gmail.com</a></p>
+            <p>WhatsApp: <a href="https://wa.me/5511956295523?text=Eu%20tenho%20uma%20ideia%20e%20preciso%20da%20sua%20ajuda!" target="_blank" rel="noreferrer">(11)95629-5523</a></p>
+            <p>Instagram: <a href="https://www.instagram.com/this.dev.gabriel/" target="_blank" rel="noreferrer">@this.dev.gabriel</a></p>
         </Element>
     )
 }
